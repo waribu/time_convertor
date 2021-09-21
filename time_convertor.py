1f19045bf08f53240ba2665d84ea778257e01879
waribu <gwaribu@gmail.com>
2021-09-21 20:41:03 +0300

    time convertor as we practise about functions and their usage
    more updates volume 2

--- time_convertor.py
+++ time_convertor.py
@@ -3,7 +3,12 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+    elif num_of_days == 0:
+        return "You entered a Zer,kindly enter a positive number"
+    else:
+         return "You enetered a negative value,so no conversion for you!"
 
 
 user_input = input ( "Hey User,Kindly enter the days you want converted into hours!\n" )
