ca3a4080169a10fcbba57cda97820c9e0f4f4786
Waribu <gwaribu@gmail.com>
2021-10-01 11:05:01 +0300

    finalizing on loops

--- time_convertor.py
+++ time_convertor.py
@@ -7,16 +7,20 @@ def days_to_units(num_of_days):
 
 
 def validate_and_execute():
-    if user_input.isdigit():
+    try:
         user_input_number = int(user_input)
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
             print("You entered a Zero,kindly enter a positive number")
-    else:
+        else:
+            print("You entered a Negative number,no conversion for you!")
+    except ValueError:
         print("Your input is not a valid number. Don't ruin my program!")
 
 
-user_input = input("Hey User,Kindly enter the days you want converted into hours!\n")
-validate_and_execute()
+user_input = ""
+while user_input != "exit":
+    user_input = input("Hey User,Kindly enter the days you want converted into hours!\n")
+    validate_and_execute()
